7126177abde11a00736309df77721be71d3eef7b
Dave Miner <wtrispsn@gmail.com>
2025-07-11 01:02:15 -0400

    adds chart page

--- src/store/reducers/amm.js
+++ src/store/reducers/amm.js
@@ -30,6 +30,9 @@ export const amm = createSlice({
     sharesLoaded: (state, action) => {
       state.shares = action.payload
     },
+    swapsLoaded: (state, action) => {
+      state.swaps = action.payload
+    },
     depositRequest: (state) => {
       state.depositing.isDepositing = true
       state.depositing.isSuccess = false
@@ -81,6 +84,7 @@ export const amm = createSlice({
 export const {
   setContract,
   sharesLoaded,
+  swapsLoaded,
   depositRequest,
   depositSuccess,
   depositFail,
